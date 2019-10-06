3460becf5dce98c3e4f894800a7edf307236ae2b
DevOrc <ncharlton002@gmail.com>
2019-10-06 09:15:26 -0400

    Add SpaceX launches page

--- src/js/spacex.js
+++ src/js/spacex.js
@@ -18,36 +18,21 @@ function insertLaunchDataIntoTable(json, table) {
     let html = [];
 
     JSON.parse(json).forEach(launch => {
+        let link = "spacex-launch.html?flight_number=" + launch.flight_number;
+
         html.push("<tr>");
         html.push("<td><b>" + launch.flight_number + "<b></td>");
         html.push("<td><b>" + launch.mission_name + "<b></td>");
         html.push("<td>" + parseMissionDate(launch.launch_date_utc) + "</td>");
-        html.push("<td><a href=\"#\" style=\"color:#6a6aFa\">Details</a></td>");
+        html.push("<td><a href=\"" + link + "\" style=\"color:#6a6aFa\">Details</a></td>");
         html.push("</tr>");
     });
 
     table.innerHTML = html.join("");
 }
 
-function parseMissionDate(date_utc_str){
+function parseMissionDate(date_utc_str) {
     let date = new Date(date_utc_str);
 
     return date.toDateString();
-}
-
-function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4) {
-            if (xmlHttp.status == 200)
-                callback(xmlHttp.responseText);
-            else
-                console.log("Request to \"" + theUrl + "\" returned error " + xmlHttp.status)
-        }
-
-    }
-    xmlHttp.open("GET", theUrl, true);
-
-    xmlHttp.send(null);
 }
