830c16af96f33380ce244999fb3e3c2cdf69cebe
DevOrc <ncharlton002@gmail.com>
2019-10-11 20:33:06 -0400

    Add scales to simulator

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -1,7 +1,10 @@
+const distanceScale = 100_000;
+const massScale = 1_000_000_000_000_000_000;
 const canvas = document.getElementById("canvas");
 const canvas_container = document.getElementById("canvas-container");
 const ctx = canvas.getContext("2d");
 let pts = [];
+let bodies = [];
 
 function onAnimationFrame() {
     syncCanvasSize();
@@ -11,95 +14,104 @@ function onAnimationFrame() {
     requestAnimationFrame(onAnimationFrame);
 }
 
+function update() {
+    bodies.forEach(b => updateBody(b));
+}
+
 function syncCanvasSize() {
     canvas.width = canvas_container.clientWidth;
     canvas.height = canvas_container.clientWidth;
 
 }
 
-function update(){
-    updateBody(body1);
-    updateBody(body2);
-
-    applyGravity(body1, body2);
-    applyGravity(body2, body1);
-}
-
 function render() {
-    renderBody(body1);
-    renderBody(body2);
-
     drawPoints();
+    bodies.forEach(b => renderBody(b));
 }
 
-function drawPoints(){
+function drawPoints() {
     pts.forEach(pt => {
-        ctx.fillStyle = "white";
+        ctx.fillStyle = pt.color;
         ctx.fillRect(pt.x, pt.y, 3, 3);
     });
 }
 
-setInterval(function(){
-    pts.push({
-        "x": body1.x,
-        "y": body1.y,
+setInterval(function () {
+    bodies.forEach(b => {
+        pts.push({
+            "x": b.x / distanceScale,
+            "y": b.y / distanceScale,
+            "color": b.color,
+        });
     });
+
 }, 1000);
 
 //
 // Physics Code
 //
 
-let body1 = createBody("Body1", 1_000, 10, 300, 400, "red");
-body1.yVel = 2.3;
-let body2 = createBody("Body2", 500_000_000_000, 25, 400, 400, "yellow");
+createBody("Earth", 10_000_000, 10, 90, 400, "green", 0, 3.2);
+createBody("Moon", 100_000, 2, 100, 400, "white", 0.0, .8 + 3.2);
+createBody("Sun", 5_000_000_000, 25, 400, 400, "yellow", 0, 0);
 
-function createBody(name, mass, radius, x, y, color){
-    if(!name || !mass || !radius || !x || !y || !color)
+function createBody(name, mass, radius, x, y, color, xVel, yVel) {
+    if (!name || !mass || !radius || !x || !y || !color)
         throw new Error("Unallowed null/undefined component");
 
-    return {
+    let body = {
         "name": name,
         "radius": radius,
-        "x": x,
-        "y": y,
-        "mass": mass,
+        "x": x * distanceScale,
+        "y": y * distanceScale,
+        "mass": mass * massScale,
         "color": color,
-        "xVel": 0.0,
-        "yVel": 0.0,
+        "xVel": xVel ? (xVel * distanceScale) : 0.0,
+        "yVel": yVel ? (yVel * distanceScale) : 0.0,
     }
+
+    bodies.push(body);
+
+    return body;
 }
 
-function applyGravity(body1, body2){
+function applyGravity(body1, body2) {
     const G = 6.675e-10;
     let xDiff = Math.abs(body1.x - body2.x);
     let yDiff = Math.abs(body1.y - body2.y);
 
     let r = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
 
-    if(r < 50){
-        return;
-    }
-
     let F = G * ((body1.mass * body2.mass) / Math.pow(r, 2));
 
     let angle = Math.atan2(yDiff, xDiff);
     let fVector = createVectorMagAngle(angle, F);
     fVector.divide(new Vector(body1.mass, body1.mass));
-    
-    body1.xVel += fVector.x * (body1.x > body2.x ? -1: 1);
-    body1.yVel += fVector.y * (body1.y > body2.y ? -1: 1);
+
+    body1.xVel += fVector.x * (body1.x > body2.x ? -1 : 1);
+    body1.yVel += fVector.y * (body1.y > body2.y ? -1 : 1);
 }
 
-function updateBody(body){
+function updateBody(body) {
+    if (!body) {
+        console.log(body);
+        return;
+    }
+
     body.x += body.xVel;
     body.y += body.yVel;
+
+    bodies.forEach(otherBody => {
+        if (body != otherBody) {
+            applyGravity(body, otherBody);
+        }
+    });
 }
 
-function renderBody(body){
+function renderBody(body) {
     ctx.fillStyle = body.color;
     ctx.beginPath();
-    ctx.ellipse(body.x, body.y, body.radius, body.radius, 0, 0, 2 * Math.PI);
+    ctx.ellipse(body.x / distanceScale, body.y / distanceScale, body.radius, body.radius, 0, 0, 2 * Math.PI);
     ctx.fill();
 }
 
@@ -107,41 +119,41 @@ function renderBody(body){
 // Vector Code
 //
 
-class Vector{
-    constructor(x, y){
+class Vector {
+    constructor(x, y) {
         this.x = x ? x : 0;
         this.y = y ? y : 0;
     }
 
-    add(vector){
+    add(vector) {
         this.x += vector.x;
         this.y += vector.y;
     }
 
-    subtract(vector){
+    subtract(vector) {
         this.x -= vector.x;
         this.y -= vector.y;
     }
 
-    divide(vector){
+    divide(vector) {
         this.x /= vector.x;
         this.y /= vector.y;
     }
 
-    getMagnitude(){
+    getMagnitude() {
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 
-    getAngle(){
+    getAngle() {
         return Math.atan2(this.y, this.x);
     }
 
-    print(){
+    print() {
         console.log("(" + this.x + ", " + this.y + ")")
     }
 }
 
-function createVectorMagAngle(angle, mag){
+function createVectorMagAngle(angle, mag) {
     return new Vector(Math.cos(angle) * mag, Math.sin(angle) * mag);
 }
 
